bad60f7654e1e8525ee14c12bd9b929b9ae8dbf1
yuri_o <51740292+yuri-jal@users.noreply.github.com>
2021-10-19 21:52:53 +0900

    20211013 과제
    7주차

--- DFS.cpp
+++ DFS.cpp
@@ -1,17 +1,45 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-bool visited[9];
-int graph[9][3] = {{}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7}};
-void dfs(int x) {
-visited[x] = true;
-cout << x << ' ';
-for (int node : graph[x]) {
-// 인접한 노드가 방문한 적이 없다면 DFS 수행
-if(!visited[node]) {
-dfs(node);
-}
-}
-}
+int n, m;
+int graph[201][201];
+// 이동할 네 가지 방향 정의 (상, 하, 좌, 우) 
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 int main(void) {
-dfs(1);
+cin >> n >> m;
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < m; j++) {
+scanf("%1d", &graph[i][j]);
+}
+}
+cout << dfs(0, 0) << '\n';
+return 0;
+}
+int dfs(int x, int y) {
+// 스택(Stack) 구현을 위해 stack 라이브러리 사용
+stack<pair<int, int> > st;
+st.push({x, y});
+// 스택이 빌 때까지 반복하기
+while(!st.empty()) {
+int x = st.top().first;
+int y = st.top().second;
+st.pop();
+// 현재 위치에서 4가지 방향으로의 위치 확인
+for (int i = 0; i < 4; i++) {
+int nx = x + dx[i];
+int ny = y + dy[i];
+// 미로 찾기 공간을 벗어난 경우 무시
+if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+// 벽인 경우 무시
+if (graph[nx][ny] == 0) continue;
+// 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+if (graph[nx][ny] == 1) {
+graph[nx][ny] = graph[x][y] + 1;
+st.push({nx, ny});
+} 
+} 
+}
+// 가장 오른쪽 아래까지의 최단 거리 반환
+return graph[n - 1][m - 1];
 }
--- DFS.java
+++ DFS.java
@@ -1,20 +1,60 @@
+import java.io.*;
 import java.util.*;
-public class Main {
-public static boolean[] visited = new boolean[9];
-static int[][] graph = {{}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7}};
-// DFS 함수 정의
-public static void dfs(int x) {
-// 현재 노드를 방문 처리
-visited[x] = true;
-System.out.print(x + " ");
-// 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-for (int node : graph[x]) {
-if (!visited[node]) {
-dfs(node);
+public class Solution {
+public static int N,M;
+public static int [][] graph = new int [200][200];
+//이동할 네 가지 방향 정의 {상, 하, 좌 ,우}
+public static int dx[] = {-1, 1, 0, 0};
+public static int dy[] = {0, 0, -1, 1};
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+// N, M 입력
+N = scanner.nextInt();
+M = scanner.nextInt();
+scanner.nextLine(); //버퍼 지우기
+System.out.println("n " + N + "m" + M);
+// 2차원 배열의 정보 입력 받기
+for(int i=0; i <N; i++){
+String str = scanner.nextLine();
+for(int j=0; j<M; j++){
+graph[i][j] = str.charAt(j) - '0';
 }
 }
+scanner.close();
+System.out.println("result = " + dfs(0,0)); 
 }
-public static void main(String[] args) {
-dfs(1);
+private static int dfs(int x, int y) 
+{ 
+Stack<Node> stack = new Stack<Node>(); 
+Node node = new Node(x, y); 
+stack.push(node); 
+while (!stack.empty()) { 
+node = stack.pop();
+x = node.getX(); 
+y = node.getY(); 
+for(int i=0;i<4;i++){
+int nx = x + dx[i];
+int ny = y + dy[i];
+//미로 범위를 벗어나면 무시
+if(nx <0 || ny < 0 || nx >= N || ny >= M ) continue;
+//한 번 왔던 위치면 무시 , 괴물이 있는 위치면 무시
+if (graph[nx][ny]==1 ){
+System.out.println("[" + nx + "," + ny + "]"); 
+graph[nx][ny] = graph[x][y] + 1;
+stack.push(new Node(nx, ny));
+}
+}
+} 
+return graph[N-1][M-1]; 
+} 
+static class Node{
+final private int x;
+final private int y;
+Node(int x, int y){
+this.x = x;
+this.y = y;
+} 
+public int getX() { return x; }
+public int getY(){ return y; }
 }
 }
